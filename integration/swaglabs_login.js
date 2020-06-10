cec34d1795872d000d8dad25ab1f9a5a76e97f4c
Leeane Gadani <leeanegadani@kevins-mbp.home>
2020-06-10 15:24:57 -0400

    updated login

--- integration/swaglabs_login.js
+++ integration/swaglabs_login.js
@@ -1,37 +1,38 @@
-it ('login user',() => {
-    cy.visit('https://www.saucedemo.com/')
-//test incorrect user name, correct password
-    cy.get('input[id="user-name"]').type('standard_user1')
-    cy.get('input[id="password"]').type('secret_sauce')
-    cy.get('input[type="submit"]').click()
+describe('Login Tests',() => {
 
-    cy.contains('Username and password do not match any user in this service')
-    cy.reload()
+    before(() => {
+        cy.visit('https://www.saucedemo.com/')
+    })
     
+    it ('login incorrect username',() => {    
+   
+        cy.get('input[id="user-name"]').type('standard_user1')
+        cy.get('input[id="password"]').type('secret_sauce')
+        cy.get('input[type="submit"]').click()
+        cy.contains('Username and password do not match any user in this service')
+        cy.reload()
+    })
 
-//test incorrect password, correct username
-    cy.get('input[id="user-name"]').type('standard_user')
-    cy.get('input[id="password"]').type('secret_sauceq')
-    cy.get('input[type="submit"]').click()
+    it ('login incorrect password',() => {
+        cy.get('input[id="user-name"]').type('standard_user')
+        cy.get('input[id="password"]').type('secret_sauceq')
+        cy.get('input[type="submit"]').click()
+        cy.contains('Username and password do not match any user in this service')
+        cy.reload() 
+    })
 
-    cy.contains('Username and password do not match any user in this service')
-    cy.reload() 
+    it ('login incorrect password and username',() => {
+        cy.get('input[id="user-name"]').type('standard_user1')
+        cy.get('input[id="password"]').type('secret_sauceq')
+        cy.get('input[type="submit"]').click()
+        cy.contains('Username and password do not match any user in this service')
+        cy.reload()
+    })
 
-//test incorrect password, incorrect username
-    cy.get('input[id="user-name"]').type('standard_user1')
-    cy.get('input[id="password"]').type('secret_sauceq')
-    cy.get('input[type="submit"]').click()
-
-    cy.contains('Username and password do not match any user in this service')
-    cy.reload()
-
-
-   //correct credentials
-    
-    cy.get('input[id="user-name"]').type('standard_user')
-    cy.get('input[id="password"]').type('secret_sauce')
-    cy.get('input[type="submit"]').click()
-
-    cy.location('pathname').should('eq', '/inventory.html')
-
+    it ('login correct password and username',() => {
+        cy.get('input[id="user-name"]').type('standard_user')
+        cy.get('input[id="password"]').type('secret_sauce')
+        cy.get('input[type="submit"]').click()
+        cy.location('pathname').should('eq', '/inventory.html')
+    })
 })
